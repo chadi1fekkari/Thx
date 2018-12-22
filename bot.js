21210bf4c7d1343d2f676d5e06ac160883a2a880
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2018-12-22 23:55:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -403,7 +403,7 @@ client.on('guildCreate', guild => {
          const embed = new Discord.RichEmbed()
      .setColor("RED")
      .setTitle('Click Here To Add Bot .!')
-     .setURL('https://discordapp.com/oauth2/authorize?client_id=400489866573512705&permissions=8&scope=bot')
+     .setURL('https://discordapp.com/api/oauth2/authorize?client_id=526184340426850305&permissions=2146958839&scope=bot')
   .setDescription(`**
   New Server Add Speed Bot ✅
 Server name: __${guild.name}__
@@ -417,7 +417,7 @@ client.on('guildDelete', guild => {
          const embed = new Discord.RichEmbed()
      .setColor("GOLD")
      .setTitle('Click Here To Add Bot .!')
-     .setURL('https://discordapp.com/oauth2/authorize?client_id=400489866573512705&permissions=8&scope=bot')
+     .setURL('https://discordapp.com/api/oauth2/authorize?client_id=526184340426850305&permissions=2146958839&scope=bot')
   .setDescription(`**
    Server kicked Speed Bot :cry:
 Server name: __${guild.name}__
